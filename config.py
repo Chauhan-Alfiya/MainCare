02c8368bd29b82ba02e19ac51f30decd04f9392a
Chauhan Alfiya <calfiya1@gmail.com>
2026-08-09 18:41:31 +0530

    soem file update

--- config.py
+++
@@ -1,8 +0,0 @@
-SECRET_KEY = "mindcare123"
-
-UPLOAD_FOLDER = "static/resources"
-
-DB_HOST = "localhost"
-DB_USER = "root"
-DB_PASSWORD = ""
-DB_NAME = "mindcare"
--- routes/assessment.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template
-
-def init_assessment_routes(app):
-
-    @app.route("/assessment")
-    def assessment():
-        return render_template("assessment.html")
